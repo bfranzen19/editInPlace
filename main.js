1e99001051eb764e9500930677dcedbcf88e9a32
bt <btfranzen@gmail.com>
2017-10-23 20:58:03 -0600

    working code for an inline text edit (edit in place)  using jquery

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+$(document).ready(function(){
+  $('.editMe').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
 
-$(document).ready(function() {
+  function divClicked() {
+    var divHtml = $(this).html();
+    var editableText = $("<textarea />");
+    editableText.val(divHtml);
+    $(this).replaceWith(editableText);
+    editableText.focus();
+    editableText.blur(editableTextBlurred);
+  }
 
+  function editableTextBlurred() {
+    var html = $(this).val();
+    var viewableText = $("<div>");
+    viewableText.html(html);
+    $(this).replaceWith(viewableText);
+    viewableText.click(divClicked);
+  }
+
+  $(document).ready(function() {
+      $("div").click(divClicked);
+  })
 
 })
